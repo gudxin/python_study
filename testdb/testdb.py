a8f505ae6365b5cc5f0bd94f5c8cfcfee18f305d
xiaogu <gdx1517253@163.com>
2019-01-24 22:59:43 +0800

    'connector'

--- testdb/testdb.py
+++ testdb/testdb.py
@@ -1,6 +1,6 @@
 import pymysql
 
-db = pymysql.connect("127.28.171.128","root","lakerkobe00","TESTDB")
+db = pymysql.connect("127.28.171.128","xiaogu","xiaogu123","mysql")
 
 cursor = db.sursor()
 
@@ -11,3 +11,13 @@ data = cursor.fetchone()
 print("Database version :{}".format(data))
 
 db.close()
+
+# import mysql.connector
+ 
+# mydb = mysql.connector.connect(
+   # host="127.28.171.128",       # 数据库主机地址
+   # user="root",    # 数据库用户名
+   # passwd="lakerkobe00"   # 数据库密码
+ # )
+ 
+# print(mydb)
