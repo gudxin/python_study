b9958903275a754bfec8d22c16711ba8a25224e1
gudxin <gdx1517253@163.com>
2019-01-25 12:19:00 +0800

    successofpymysql

--- testdb/testdb.py
+++ testdb/testdb.py
@@ -1,13 +1,26 @@
 import pymysql
 
-db = pymysql.connect("127.28.171.128","root","lakerkobe00","TESTDB")
+db = pymysql.connect("192.168.14.130","xiaogu","lakerkobe00","TESTDB")
 
-cursor = db.sursor()
+cursor = db.cursor()
 
-cursor.execute("SELECT VERSION()")
-
-data = cursor.fetchone()
-
-print("Database version :{}".format(data))
+sql = """INSERT INTO ZIGBEE(HEAD,
+		IP,PH,CONDI,TEMP1,TEMP2,TEMP3,TEMP4,TEMP5,TEMP6,TEMP7,TEMP8,
+		PAUSE1,PAUSE2,PAUSE3,PAUSE4,PAUSE5,SPEED,THICKNESS,SUM,DATE,TAIL)
+		VALUES ('ff', '172017000255', '00.00', '000.0', '617.0',
+		'617.0','000.0','000.0','000.0','000.0','000.0','000.0',
+		'1','1','1','1','1','00.00','0000','185','20190123083734','11')"""
+		
+try:
+	#执行sql语句
+   cursor.execute(sql)
+	#提交到数据库执行
+   db.commit()
+except:
+	#如果发生错误则回滚
+   db.rollback()
+   
+# cursor.execute(sql)
 
 db.close()
+
