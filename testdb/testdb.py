9fe34e1ba67dd490d358d3ff08fdb1b811026d77
xiaogu <gdx1517253@163.com>
2019-01-28 19:13:03 +0800

    testdb

--- testdb/testdb.py
+++ testdb/testdb.py
@@ -1,23 +1,23 @@
-import pymysql
+# import pymysql
 
-db = pymysql.connect("127.28.171.128","xiaogu","xiaogu123","mysql")
+# db = pymysql.connect("127.28.171.128","xiaogu","xiaogu123","mysql")
 
-cursor = db.sursor()
+# cursor = db.sursor()
 
-cursor.execute("SELECT VERSION()")
+# cursor.execute("SELECT VERSION()")
 
-data = cursor.fetchone()
+# data = cursor.fetchone()
 
-print("Database version :{}".format(data))
+# print("Database version :{}".format(data))
 
-db.close()
+# db.close()
 
-# import mysql.connector
+import mysql.connector
  
-# mydb = mysql.connector.connect(
-   # host="127.28.171.128",       # 数据库主机地址
-   # user="root",    # 数据库用户名
-   # passwd="lakerkobe00"   # 数据库密码
- # )
+mydb = mysql.connector.connect(
+host="127.28.171.128",       # 数据库主机地址
+user="root",    # 数据库用户名
+passwd="lakerkobe00"   # 数据库密码
+)
  
-# print(mydb)
+print(mydb)
