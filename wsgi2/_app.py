9a566e9034f3e846cada43d2019df53681fd35de
gudxin <gdx1517253@163.com>
2019-02-15 17:04:00 +0800

    'wsgi2'

---
+++ wsgi2/_app.py
@@ -0,0 +1,6 @@
+#hello.py
+def application(environ, start_response):
+    start_response("200 OK",[('Content-Type', 'text/html')])
+    # return [b'<h1>Hello,Xiao gu!</h1>']
+    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return [body.encode('utf-8')]
